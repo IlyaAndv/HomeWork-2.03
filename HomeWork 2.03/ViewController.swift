9a6773ad9416a8d1423a58d3d6d4c71a02efa038
Ilya <ktoto47913@gmail.com>
2023-07-30 09:01:53 +0300

    Mowing objects View Controller

--- HomeWork 2.03/ViewController.swift
+++ HomeWork 2.03/ViewController.swift
@@ -2,11 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorToneDisplay: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        colorToneDisplay.layer.cornerRadius = 27
     }
-
-
+    
 }
 
